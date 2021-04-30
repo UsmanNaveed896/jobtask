8468be176dcfe2c3a640b4537f816ae3473736e0
usmannaveed896 <usmann207@gmail.com>
2021-04-30 23:59:55 +0500

    usman

---
+++ src/components/to-do/todo.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "./todo.css";
+export default () => {
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+  const [editingText, setEditingText] = React.useState("");
+  const [editId, setEditId] = useState();
+  function handleSubmit(e) {
+    let random = Math.random() * 49 + 1;
+    e.preventDefault();
+    let dup = [...todos];
+    dup.push({
+      title: todo,
+      id: parseInt(random),
+    });
+    setTodos(dup);
+  }
+  const handleChange = (value) => {
+    setTodo(value);
+  };
+
+  const submitEdits = (id) => {
+    // setEditId(id);
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.title = editingText;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    setEditId(null);
+  };
+  const handleDelete = (id) => {
+    const duplicate = todos.filter((sin) => sin.id !== id);
+    setTodos(duplicate);
+  };
+  return (
+    <>
+      <div id="todo-list">
+        <h1>Todo List</h1>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={(e) => {
+              handleChange(e.target.value);
+            }}
+          />
+          <button type="submit">Add Todo</button>
+        </form>
+        {todos &&
+          todos.length !== 0 &&
+          todos.map((single) => (
+            <div className="todo">
+              <div className="todo-text">
+                <input
+                  type="checkbox"
+                  id="completed"
+                  checked={todo.completed}
+                />
+                {single.id !== editId && <p>{single.title}</p>}
+                {single.id === editId ? (
+                  <input
+                    type="text"
+                    onChange={(e) => {
+                      setEditingText(e.target.value);
+                    }}
+                  />
+                ) : (
+                  <div></div>
+                )}
+              </div>
+              <div className="todo-actions">
+                <button onClick={() => setEditId(single.id)}>Edit</button>
+                <button onClick={() => submitEdits(single.id)}>Complete</button>
+
+                <button
+                  onClick={() => {
+                    handleDelete(single.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
